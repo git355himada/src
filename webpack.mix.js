c302f3e2742739edfdd62113d2d3baf389435d1c
vagrant <vagrant@localhost.localdomain>
2021-01-01 19:36:25 +0000

    aaa

---
+++ webpack.mix.js
@@ -0,0 +1,38 @@
+const mix = require('laravel-mix');
+var HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.react('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .webpackConfig({
+        plugins: [
+            new HardSourceWebpackPlugin()
+        ],
+        devtool: "source-map",
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    enforce: "pre",
+                    test: /\.js$/,
+                    loader: "source-map-loader"
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js'],
+        },
+    });
